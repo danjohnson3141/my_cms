f5a0cddfd7db960fa65508095ca33ea36a4b9a28
Dan Johnson <drj3141@gmail.com>
2014-05-15 20:15:36 -0700

    initial commit
    Signed-off-by: Dan Johnson <drj3141@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCms::Application.config.secret_token = 'db2869f94a29476e3b15c5e5bbf17a3fb11ecd240dbd520e7736c9dd53c1b6bd511eb075ce6f0651c7a80fa2159701fc7c6418d95050eef84d40a17ff4f74dc9'
